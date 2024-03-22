3adb0eae1298b16d309f0b6325af727ce7af0abc
zhangzhi532200 <985691579@qq.com>
2024-03-22 11:17:24 +0800

    V1.0.0

---
+++ Assets/UnityLitJson/Editor/Converter.cs
@@ -0,0 +1,84 @@
+﻿using LitJson;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Unity.Plastic.Newtonsoft.Json;
+using System.Collections.Generic;
+
+public class Converter
+{
+    private static readonly string JsonFileName = "JsonText.json";
+    private static readonly string JsonFilePath = Path.Combine(Application.dataPath, "UnityLitJson", JsonFileName).Replace('\\', '/');
+    private static string FileJSONString;
+    private static JsonData FileJSONData;
+
+    static Converter()
+    {
+        if (!File.Exists(JsonFilePath))
+        {
+            Debug.LogError($"{JsonFileName}文件不存在!");
+            return;
+        }
+        FileJSONString = File.ReadAllText(JsonFilePath);
+        FileJSONData = JsonMapper.ToObject<JsonData>(FileJSONString);
+    }
+
+    [MenuItem("Converter/序列化为JsonString")]
+    public static void ToJsonString()
+    {
+        string json = FileJSONData.ToJson();
+        //不带格式缩进
+        Debug.Log($"<color={"yellow"}>{json}</color>");
+        //带格式缩进
+        Debug.Log(JsonConvert.SerializeObject(JsonConvert.DeserializeObject(json), Formatting.Indented));
+    }
+
+    /// <summary>
+    /// user结构体
+    /// </summary>
+    /*
+     {
+      "users": [
+        {
+          "id": 1,
+          "name": "Alice",
+          "age": 25,
+          "email": "alice@example.com",
+          "address": {
+            "street": "123 Main St",
+            ...
+          },
+          "interests": [
+            "reading",
+            ...
+          ]
+        },
+        ...
+    }
+     */
+    [MenuItem("Converter/序列化为JsonObject")]
+    public static void Trans2ScriptableObject()
+    {
+        string Title = "JsonObject：";
+        JsonData jsonData = JsonMapper.ToObject(FileJSONString);
+        Debug.Log($"<color={"red"}>{Title}</color><color={"yellow"}>{jsonData.ToJson()}</color>");
+        IList<JsonData> JSONList = jsonData["users"].ValueAsArray();
+        foreach (JsonData item in JSONList)
+        {
+            ColorLog(item["id"].ValueAsInt(), "green");
+            ColorLog(item["name"].ValueAsString(), "green");
+            ColorLog(item["email"].ValueAsString(), "green");
+            IList<JsonData> interestsList = item["interests"].ValueAsArray();
+            foreach (string interests in interestsList)
+            {
+                ColorLog(interests, "green");
+            }
+        }
+
+    }
+
+    public static void ColorLog(object obj, string color)
+    {
+        Debug.Log($"<color={color}>{obj}</color>");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # UnityLitJson
+适用于Unity的LitJson库 
+
+简介
+----
+基于[原生的LitJson库](https://github.com/LitJSON/litjson)修改的适用于Unity的LitJson库。
+
+使用方法 
+----
+**通过 Unity Package 安装**
+- 在 [Releases](https://github.com/zhangzhi532200/UnityLitJson/releases) 页面中，下载最新版本的 `UnityLitJson.unitypackage` 安装包，然后导入到您的项目中。
+
+**通过 Package Manager 安装**
+- 在 Unity 菜单栏中打开 `Window/Package Manager`，点击左上角 `+` 号，选择 `Add package from git URL...` ，输入 `https://github.com/zhangzhi532200/UnityLitJson.git#upm` 并确认。
+
+测试用例
+---
+![image](https://github.com/zhangzhi532200/UnityLitJson/blob/main/img/1.png)
